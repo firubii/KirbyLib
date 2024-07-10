fa181232710c00948d00753fbc886815ef698902
firubii <fruitymage@gmail.com>
2024-07-10 15:37:27 -0500

    Kirby Star Allies Map3D support

---
+++ KirbyLib/Map3D.cs
@@ -0,0 +1,163 @@
+﻿using KirbyLib.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirbyLib
+{
+    /// <summary>
+    /// A 3D collision file for Kirby Star Allies's world maps.
+    /// </summary>
+    public class Map3D
+    {
+        public class Mesh
+        {
+            public string Name;
+            public List<Triangle> Triangles = new List<Triangle>();
+        }
+
+        public struct Triangle
+        {
+            public Vector3 Vertex1;
+            public Vector3 Vertex2;
+            public Vector3 Vertex3;
+            public Vector3 Normal;
+        }
+
+        public const uint MAGIC_NUMBER = 0x18;
+
+        public XData XData { get; private set; } = new XData()
+        {
+            Version = new byte[] { 4, 0 },
+            Endianness = Endianness.Little
+        };
+
+        public List<Mesh> Meshes = new List<Mesh>();
+
+        public int Unknown1;
+        public int Unknown2;
+        public int Unknown3;
+        public int Unknown4;
+        public int Unknown5;
+        public int Unknown6;
+
+        public void Read(EndianBinaryReader reader)
+        {
+            XData.Read(reader);
+
+            uint magic = reader.ReadUInt32();
+            if (magic != MAGIC_NUMBER)
+                throw new InvalidDataException($"Expected magic {MAGIC_NUMBER}, got {magic}");
+
+            uint meshSection = reader.ReadUInt32();
+            uint generalSection = reader.ReadUInt32();
+
+            reader.BaseStream.Position = meshSection;
+            Meshes = new List<Mesh>();
+            uint meshCount = reader.ReadUInt32();
+            for (uint i = 0; i < meshCount; i++)
+            {
+                Mesh mesh = new Mesh();
+
+                reader.BaseStream.Position = meshSection + 4 + (i * 8);
+                uint triSection = reader.ReadUInt32();
+                mesh.Name = reader.ReadStringOffset();
+
+                reader.BaseStream.Position = triSection;
+                mesh.Triangles = new List<Triangle>();
+                uint triCount = reader.ReadUInt32();
+                for (int t = 0; t < triCount; t++)
+                {
+                    Triangle tri = new Triangle();
+                    tri.Vertex1 = reader.ReadVector3();
+                    tri.Vertex2 = reader.ReadVector3();
+                    tri.Vertex3 = reader.ReadVector3();
+                    tri.Normal = reader.ReadVector3();
+
+                    mesh.Triangles.Add(tri);
+                }
+            }
+
+            reader.BaseStream.Position = generalSection;
+            Unknown1 = reader.ReadInt32();
+            Unknown2 = reader.ReadInt32();
+            Unknown3 = reader.ReadInt32();
+            Unknown4 = reader.ReadInt32();
+            Unknown5 = reader.ReadInt32();
+            Unknown6 = reader.ReadInt32();
+        }
+
+        public void Write(EndianBinaryWriter writer)
+        {
+            XData.WriteHeader(writer);
+
+            StringHelperContainer strings = new StringHelperContainer();
+
+            long headerStart = writer.BaseStream.Position;
+
+            writer.Write(MAGIC_NUMBER);
+            writer.Write(-1);
+            writer.Write(-1);
+            writer.Write(-1);
+
+            long meshSection = writer.BaseStream.Position;
+            writer.WritePositionAt(headerStart + 0x4);
+            writer.Write(Meshes.Count);
+            for (int i = 0; i < Meshes.Count; i++)
+            {
+                writer.Write(-1);
+                strings.Add(writer.BaseStream.Position, Meshes[i].Name);
+                writer.Write(-1);
+            }
+
+            writer.WritePositionAt(headerStart + 0x8);
+            writer.Write(Unknown1);
+            writer.Write(Unknown2);
+            writer.Write(Unknown3);
+            writer.Write(Unknown4);
+            writer.Write(Unknown5);
+            writer.Write(Unknown6);
+
+            for (int i = 0; i < Meshes.Count; i++)
+            {
+                var mesh = Meshes[i];
+
+                writer.WritePositionAt(meshSection + 4 + (i * 8));
+                writer.Write(mesh.Triangles.Count);
+                for (int t = 0; t < mesh.Triangles.Count; t++)
+                {
+                    writer.Write(mesh.Triangles[i].Vertex1);
+                    writer.Write(mesh.Triangles[i].Vertex2);
+                    writer.Write(mesh.Triangles[i].Vertex3);
+                    writer.Write(mesh.Triangles[i].Normal);
+                }
+            }
+
+            writer.WritePositionAt(headerStart + 0xC);
+            writer.Write(0);
+            writer.Write(0);
+            writer.Write(0);
+            writer.Write(writer.BaseStream.Position + 0x8);
+            writer.Write(0);
+            writer.Write(0);
+
+            writer.Write(Meshes.Sum(x => x.Triangles.Count));
+            for (int i = 0; i < Meshes.Count; i++)
+            {
+                for (int t = 0; t < Meshes[i].Triangles.Count; t++)
+                {
+                    writer.Write(i);
+                    writer.Write(t);
+                }
+            }
+
+            strings.WriteAll(writer);
+
+            XData.WriteFilesize(writer);
+            XData.WriteFooter(writer);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,32 +5,33 @@
 All sections of all formats are fully supported.
 
 * **2D Map Files** for the following games:
-	* Kirby's Return to Dream Land
-	* Kirby's Dream Collection
-	* Kirby: Triple Deluxe
-	* Kirby Fighters
-	* Kirby Fighters Deluxe
-	* Kirby: Planet Robobot
-	* Team Kirby Clash
-	* Team Kirby Clash Deluxe
-	* Kirby Star Allies
-	* Super Kirby Clash
-	* Kirby Fighters 2
-	* Kirby's Return to Dream Land Deluxe
+	* [Kirby's Return to Dream Land](KirbyLib/Mapping/MapRtDL.cs)
+	* [Kirby's Dream Collection](KirbyLib/Mapping/MapRtDL.cs)
+	* [Kirby: Triple Deluxe](KirbyLib/Mapping/MapTDX.cs)
+	* [Kirby Fighters](KirbyLib/Mapping/MapFighters.cs)
+	* [Kirby Fighters Deluxe](KirbyLib/Mapping/MapFighters.cs)
+	* [Kirby: Planet Robobot](KirbyLib/Mapping/MapKPR.cs)
+	* [Team Kirby Clash](KirbyLib/Mapping/MapClash.cs)
+	* [Team Kirby Clash Deluxe](KirbyLib/Mapping/MapClashDeluxe.cs)
+	* [Kirby Star Allies](KirbyLib/Mapping/MapKSA.cs)
+	* [Super Kirby Clash](KirbyLib/Mapping/MapClashSuper.cs)
+	* [Kirby Fighters 2](KirbyLib/Mapping/MapFighters.cs)
+	* [Kirby's Return to Dream Land Deluxe](KirbyLib/Mapping/MapRtDL.cs)
+* [**3D Map Files**](KirbyLib/Mapping/Map3D.cs) for **Kirby Star Allies** world maps
 * **Cinemo Dynamics** (CND)
-	* The early Kirby Star Allies and Super Kirby Clash iteration is handled separately from the one introduced in Kirby Fighters 2 for cleanliness and ease of use.
-* **FDG Preload Files**
+	* The [early Kirby Star Allies and Super Kirby Clash iteration](KirbyLib/CinemoKSA.cs) is handled separately from [the one introduced in Kirby Fighters 2](KirbyLib/Cinemo.cs) for cleanliness and ease of use.
+* [**FDG Preload Files**](KirbyLib/FDG.cs)
 	* Version 2 (Kirby's Return to Dream Land to Kirby Star Allies)
 	* Version 3 (Kirby and the Forgotten Land and later)
-* **Generic Archives**
+* [**Generic Archives**](KirbyLib/GenericArchive.cs)
 	* Used as a generic container across HAL Laboratory games, usually for parameter files (i.e. Kirby's Return to Dream Land)
-* **Msg Filter**
+* [**Msg Filter**](KirbyLib/MsgFilter.cs)
 	* Used to control which glyphs fonts will load.
-* **Yaml**
+* [**Yaml**](KirbyLib/Yaml.cs)
 	* Version 2 (Kirby: Planet Robobot to Kirby Fighters 2)
 	* Version 4 (Kirby and the Forgotten Land and Kirby's Dream Buffet)
 	* Version 5 (Kirby's Return to Dream Land Deluxe)
-* **XData Header**
+* [**XData Header**](KirbyLib/XData.cs)
 	* Version 2.0 (Kirby's Return to Dream Land to Team Kirby Clash Deluxe)
 	* Version 4.0 (Kirby Battle Royale to Kirby Fighters 2)
 	* Version 5.0 (Kirby and the Forgotten Land and later)
